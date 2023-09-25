af91c300d73ef8e58ced9d1784f5eb4145e879d7
Nacho Villanueva <ignacio@helmi.la>
2023-09-24 21:15:26 -0300

    Docker Compose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {Controller, Get, Res, Session} from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
@@ -16,13 +16,29 @@ export class AppController {
     return res.json(await this.appService.createDID());
   }
 
-    @Get('publish')
-    async publishDID(@Res() res: Response): Promise<any> {
-      return res.json(await this.appService.publishDID());
-    }
+  @Get('publish')
+  async publishDID(@Res() res: Response): Promise<any> {
+    return res.json(await this.appService.publishDID());
+  }
+
+  @Get('did/generate')
+  async generateDID(@Res() res: Response):Promise<any> {
+    return res.json(await this.appService.publishDID())
+  }
+
+  @Get('test2')
+  async test2(@Res() res: Response):Promise<any> {
+    return res.json("asd");
+  }
 
   @Get("request-credential")
   async requestCredential(@Res() res: Response) {
     return res.json(await this.appService.requestCreateCredential());
+  }
+
+  @Get("test")
+  test(@Session() session: Record<string, any>, @Res() res: Response){
+    session.test = session.test ? session.test + 1 : 1;
+    return res.json(session.test);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {createClient} from "redis";
+import RedisStore from "connect-redis"
+import * as session from "express-session";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const redisHost = process.env.REDIS_HOST || 'redis';
+    const redisPort = process.env.REDIS_PORT || 6379;
+
+    // Initialize client.
+    let redisClient = createClient({
+        url: `redis://${redisHost}:${redisPort}`
+    })
+    redisClient.connect().catch(console.error)
+
+    let redisStore = RedisStore
+
+    app.use(
+        session({
+            //@ts-ignore
+            store: new redisStore({
+                //@ts-ignore
+                client: redisClient,
+                prefix: "vcnft"
+            }),
+            resave: false, // required: force lightweight session keep alive (touch)
+            saveUninitialized: false, // recommended: only save session when data exists
+            secret: "keyboard cat",
+        })
+    )
+
+    await app.listen(3000, "0.0.0.0");
 }
+
 bootstrap();
